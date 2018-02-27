7c6e3197d0dc7a16c34e8f0ba30c2b61698b4879
Estêvão Samuel Procópio Amaral <tevaum@gmail.com>
2018-02-27 09:53:25 -0300

    Refactoring LoginHelper

--- helper/login.js
+++ helper/login.js
@@ -1,6 +1,6 @@
 var mysql = null;
 
-module.exports = {
+var Login = {
     init: function(_mysql) {
 	mysql = _mysql;
     },
@@ -14,16 +14,26 @@ module.exports = {
 	    callback({status: 'error', msg: 'Usuário ou senha inválidos.'});
 	    return;
 	} else if (user.adm_global == 1  || (user.adm_local == 1 && user.cliente_status == 1)) {
-	    let menu_access_sql = 'SELECT m.descricao as menu, sm.descricao as submenu, p.nivel as access from sys_menu_permissao p JOIN sys_submenu sm USING (idsys_submenu) JOIN sys_menu m USING (idsys_menu) WHERE idcad_usuario = ?';
-	    let menu_access = await mysql.exec(menu_access_sql, [user.idcad_usuario]);
+	    Login.access(user.email, (login, access_data) => {
+		console.log('[Login] Retrieved access data for user', login + ':', access_data);
+		callback({login: user.email, cache: access_data});
+	    });
 
-	    let api_access_sql = 'SELECT m.api_url as endpoint, p.nivel as access from sys_menu_permissao p JOIN sys_apimap m USING (idsys_submenu) WHERE idcad_usuario = ?';
-	    let api_access = await mysql.exec(api_access_sql, [user.idcad_usuario]);
-	    
-	    callback({login: user.email, cache: {menu: menu_access, api: api_access}});
 	} else {
 	    callback({status: 'error', msg: 'Acesso negado.'});
 	    return;	    
 	}
+    },
+
+    access: async function(login, callback) {
+	let menu_access_sql = 'SELECT m.descricao as menu, sm.descricao as submenu, p.nivel as access from sys_menu_permissao p JOIN sys_submenu sm USING (idsys_submenu) JOIN sys_menu m USING (idsys_menu) JOIN cad_usuario u USING (idcad_usuario) WHERE u.email = ?';
+	let menu_access = await mysql.exec(menu_access_sql, [login]);
+
+	let api_access_sql = 'SELECT m.api_url as endpoint, p.nivel as access from sys_menu_permissao p JOIN sys_apimap m USING (idsys_submenu) JOIN cad_usuario u USING (idcad_usuario) WHERE u.email = ?';
+	let api_access = await mysql.exec(api_access_sql, [login]);
+
+	callback(login, {menu: menu_access, api: api_access});
     }
 };
+
+module.exports = Login;
